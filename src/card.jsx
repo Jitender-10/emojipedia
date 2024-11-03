06c241ba05fc9c0dd8e3626be52d153777804e0e
Jitender <158012976+Jitender-10@users.noreply.github.com>
2024-11-03 12:38:03 +0530

    Add files via upload

---
+++ src/card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import emojiInfo from "./emojiData.jsx";
+
+
+function Card(props) {
+    return (
+        <div className="term">
+            <dt>
+                <span className="emoji" role="img" aria-label="{props.name}">
+                    {props.emoji}
+                </span>
+                <span>{props.name}</span>
+            </dt>
+            <dd>{props.meaning}</dd>
+        </div>
+
+    );
+
+}
+
+export default Card;
---
+++ src/emojiData.jsx
@@ -0,0 +1,31 @@
+const emojiInfo = [
+    {
+        id:1,
+        emoji:"💀",
+        name:"Skull",
+        meaning:"A whitish-gray, cartoon-styled human skull with large, black eye sockets. Commonly expresses figurative death",
+    },
+    {
+        id:2,
+        emoji:"😵",
+        name:"Face with Crossed-Out Eyes",
+        meaning:"A yellow face with X’s for eyes and a round, open mouth. Often depicted with raised or furrowed eyebrows. Facebook’s design features a purple forehead. Previously displayed with spiral eyes on some platforms",
+
+    },
+    {
+        id:3,
+        emoji:"😍",
+        name:"Smiling Face with Heart-Eyes",
+        meaning:"A yellow face with an open smile, sometimes showing teeth, and red, cartoon-styled hearts for eyes. Often conveys enthusiastic feelings of love, infatuation, and adoration",
+
+    },
+    {
+        id:4,
+        emoji:"🔥",
+        name:"Fire",
+        meaning:"A flame, as produced when something is on fire. Depicted as a red, orange, and yellow flickering flame",
+    }
+
+];
+
+export default emojiInfo;
